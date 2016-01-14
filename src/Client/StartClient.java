bbbfb0f2e75fbc4a0e3b3c12223a7c91b91fc90b
Williamxu98 <xuwilliam01@gmail.com>
2016-01-14 17:39:08 -0500

    Removed unnecessary eventqueue in startClient

--- src/Client/StartClient.java
+++ src/Client/StartClient.java
@@ -17,69 +17,63 @@ public class StartClient
 	public static void main(String[] args)
 	{
 
-		EventQueue.invokeLater(new Runnable()
+		boolean connected = false;
+		int port;
+
+		String serverIP = JOptionPane
+				.showInputDialog("Please enter the IP address of the server");
+		if (serverIP.equals(""))
 		{
-			public void run()
+			serverIP = "192.168.0.16";
+			port = 5000;
+		}
+		else
+		{
+			port = Integer.parseInt(JOptionPane
+					.showInputDialog("Please enter the port of the server"));
+		}
+
+		Socket mySocket = null;
+
+		while (!connected)
+		{
+			try
 			{
-				boolean connected = false;
-				int port;
-
-				String serverIP = JOptionPane
-						.showInputDialog("Please enter the IP address of the server");
-				if (serverIP.equals(""))
-				{
-					serverIP = "192.168.0.16";
-					port = 5000;
-				}
-				else
-				{
-					port = Integer.parseInt(JOptionPane
-							.showInputDialog("Please enter the port of the server"));
-				}
-
-				Socket mySocket = null;
-
-				while (!connected)
-				{
-					try
-					{
-						mySocket = new Socket(serverIP, port);
-						connected = true;
-					}
-					catch (IOException e)
-					{
-						serverIP = JOptionPane
-								.showInputDialog("Connection Failed. Please a new IP");
-						port = Integer.parseInt(JOptionPane
-								.showInputDialog("Please enter the port of the server"));
-					}
-				}
-				ClientFrame myFrame = new ClientFrame();
-				JLayeredPane pane = new JLayeredPane();
-				pane.setLocation(0,0);
-				pane.setLayout(null);
-				pane.setSize(Client.SCREEN_WIDTH, Client.SCREEN_HEIGHT);
-				myFrame.add(pane);
-				pane.setVisible(true);
-				
-				ClientInventory inventory = new ClientInventory();
-				Client client = new Client(mySocket,inventory,pane);
-				inventory.setClient(client);
-				
-				client.setLocation(0,0);
-				inventory.setLocation(Client.SCREEN_WIDTH,0);
-				
-				pane.add(client);
-				myFrame.add(inventory);
-				client.initialize();
-				client.revalidate();
-				inventory.revalidate();
-				pane.revalidate();
-				pane.setVisible(true);
-				myFrame.setVisible(true);
-				inventory.repaint();
+				mySocket = new Socket(serverIP, port);
+				connected = true;
 			}
-		});
+			catch (IOException e)
+			{
+				serverIP = JOptionPane
+						.showInputDialog("Connection Failed. Please a new IP");
+				port = Integer.parseInt(JOptionPane
+						.showInputDialog("Please enter the port of the server"));
+			}
+		}
+		ClientFrame myFrame = new ClientFrame();
+		JLayeredPane pane = new JLayeredPane();
+		pane.setLocation(0,0);
+		pane.setLayout(null);
+		pane.setSize(Client.SCREEN_WIDTH, Client.SCREEN_HEIGHT);
+		myFrame.add(pane);
+		pane.setVisible(true);
+		
+		ClientInventory inventory = new ClientInventory();
+		Client client = new Client(mySocket,inventory,pane);
+		inventory.setClient(client);
+		
+		client.setLocation(0,0);
+		inventory.setLocation(Client.SCREEN_WIDTH,0);
+		
+		pane.add(client);
+		myFrame.add(inventory);
+		client.initialize();
+		client.revalidate();
+		inventory.revalidate();
+		pane.revalidate();
+		pane.setVisible(true);
+		myFrame.setVisible(true);
+		inventory.repaint();
 
 	}
 }
