0b77044e96a7dd07abc165014f8f4c8a9d13930c
Williamxu98 <xuwilliam01@gmail.com>
2016-01-17 22:46:32 -0500

    Made playable world map

--- src/Client/ClientWorld.java
+++ src/Client/ClientWorld.java
@@ -118,7 +118,7 @@ public class ClientWorld
 		}
 
 		clouds = new ArrayList<ClientBackground>();
-		for (int no = 0; no < 0; no++)
+		for (int no = 0; no < 12; no++)
 		{
 			double x = Client.SCREEN_WIDTH / 2 + Math.random() * CLOUD_DISTANCE
 					- (CLOUD_DISTANCE / 2);
