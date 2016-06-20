3dab89c421ed4f7c7e7b3294a955944cc247fbbf
Williamxu98 <xuwilliam01@gmail.com>
2016-06-19 22:02:27 -0400

    Added more clouds

--- src/Client/ClientWorld.java
+++ src/Client/ClientWorld.java
@@ -128,7 +128,7 @@ public class ClientWorld
 		}
 
 		clouds = new ArrayList<ClientBackground>();
-		for (int no = 0; no < 8; no++)
+		for (int no = 0; no < 12; no++)
 		{
 			double x = Client.SCREEN_WIDTH / 2 + Math.random() * CLOUD_DISTANCE
 					- (CLOUD_DISTANCE / 2);
