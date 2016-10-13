5beecae7daf4f4aa12233bbea7423d7803b3daf6
Williamxu98 <xuwilliam01@gmail.com>
2016-10-12 22:32:10 -0400

    Changed damage text yellow back to the original yellow

--- Source/Imports/Images.java
+++ Source/Imports/Images.java
@@ -30,7 +30,7 @@ import WorldCreator.CreatorObject;
  */
 public class Images {
 	
-	public final static Color YELLOW_TEXT = new Color(204, 204, 0);
+	public final static Color YELLOW_TEXT = new Color(204, 153, 0);
 	public final static Color RED_TEXT = new Color(153, 0, 38);
 	public final static Color BLUE_TEXT = new Color(0, 161, 230);
 	public final static Color GREEN_TEXT = new Color(0, 153, 0);
