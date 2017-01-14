41b011cd513cfb17de785ace0e7d1857b96d0daf
Williamxu98 <xuwilliam01@gmail.com>
2017-01-14 18:46:11 -0500

    Fixed player not disconnecting issue

--- Source/Server/Creatures/ServerPlayer.java
+++ Source/Server/Creatures/ServerPlayer.java
@@ -1135,8 +1135,6 @@ public class ServerPlayer extends ServerCreature implements Runnable {
 			try {
 				// Read the next line the player sent in
 				String command = input.readLine();
-				if(command == null)
-					continue;
 				
 				String[] tokens = command.split(" ");
 
