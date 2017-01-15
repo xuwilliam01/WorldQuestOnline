e58fbaefca43a0cd9abee52835e91521fb255aec
Williamxu98 <xuwilliam01@gmail.com>
2017-01-15 15:22:25 -0500

    Fixed building placement bug

--- Source/Imports/Images.java
+++ Source/Imports/Images.java
@@ -767,7 +767,7 @@ public class Images {
 
 			addtoImageArray(new GameImage("WorldQuestIcon.png", "Interface////"));
 			
-			addtoImageArray(new GameImage("menuBackground.png", ClientFrame.getScaledWidth(1920), ClientFrame.getScaledHeight(1080), "Interface////"));
+			addtoImageArray(new GameImage("menuBackground.png", ClientFrame.getScaledWidth(1920+ ClientInventory.INVENTORY_WIDTH), ClientFrame.getScaledHeight(1080), "Interface////"));
 
 			addtoImageArray(new GameImage("BRICK.png", ServerWorld.TILE_SIZE,
 					ServerWorld.TILE_SIZE, "Tiles//"));
