154b1a2edfe07b5a135bc12281d56588ac18e84a
Alex <alex.raita@gmail.com>
2017-01-14 21:10:07 -0500

    Changed leaderboard back to working stage

--- Source/ClientUDP/Leaderboard.java
+++ Source/ClientUDP/Leaderboard.java
@@ -4,8 +4,6 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.FocusEvent;
-import java.awt.event.FocusListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.awt.event.WindowEvent;
@@ -17,24 +15,20 @@ import java.net.InetAddress;
 import java.net.SocketException;
 import java.util.ArrayList;
 
-import javax.swing.GroupLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JScrollPane;
-import javax.swing.JTable;
 import javax.swing.JTextField;
-import javax.swing.table.TableModel;
 
 import CentralServer.LeaderboardPlayer;
 import Client.Client;
 import Client.ClientInventory;
 import Menu.MainMenu;
 
-public class Leaderboard extends JFrame implements Runnable, ActionListener, WindowListener, KeyListener, FocusListener{
+public class Leaderboard extends JFrame implements Runnable, ActionListener, WindowListener, KeyListener{
 
-	public static final int LEADERBOARD_DISPLAY_SIZE = 10;
+	public static final int LEADERBOARD_DISPLAY_SIZE = 20;
 
 	private DatagramSocket socket;
 	private DatagramPacket receive;
@@ -45,27 +39,13 @@ public class Leaderboard extends JFrame implements Runnable, ActionListener, Win
 
 	public static boolean open = false;
 
-	private JButton refresh;
-	private JButton back;
+	private JButton refresh = new JButton("Refresh");
 
 	private ArrayList<LeaderboardPlayer> leaderboard = new ArrayList<LeaderboardPlayer>();
 	private ArrayList<LeaderboardPlayer> displayboard = new ArrayList<LeaderboardPlayer>();
 
-	private JTextField search;
-	private JLabel searchL;
-	private JLabel title;
-	
-	private JTable table;
-	
-	
-	JScrollPane scrollPane;
-	
-	private String[] columnNames = {"Rank",
-			"Name",
-            "Rating",
-            "Wins",
-            "Losses"};
-	Object[][] data;
+	private JTextField search = new JTextField();
+	private JLabel searchL = new JLabel("Search");
 
 	public Leaderboard(int port) throws SocketException
 	{
@@ -79,8 +59,6 @@ public class Leaderboard extends JFrame implements Runnable, ActionListener, Win
 		setVisible(true);
 		revalidate();
 		addWindowListener(this);
-		
-		
 
 		socket = new DatagramSocket(port);
 		receiveData = new byte[1024];
@@ -92,7 +70,6 @@ public class Leaderboard extends JFrame implements Runnable, ActionListener, Win
 		panel.setSize(getWidth(),getHeight());
 		panel.setLocation(0,0);
 		add(panel);
-		
 	}
 
 	@Override
@@ -150,8 +127,8 @@ public class Leaderboard extends JFrame implements Runnable, ActionListener, Win
 	}
 
 	@Override
-	public void actionPerformed(ActionEvent event) {
-		if(event.getSource() == refresh)
+	public void actionPerformed(ActionEvent arg0) {
+		if(arg0.getSource() == refresh)
 		{
 			sendData = "B".getBytes();
 			try {
@@ -161,12 +138,6 @@ public class Leaderboard extends JFrame implements Runnable, ActionListener, Win
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-			updateTable();
-		}
-		if(event.getSource() == back)
-		{
-			setVisible(false);
-			dispose();
 		}
 	}
 
@@ -197,81 +168,33 @@ public class Leaderboard extends JFrame implements Runnable, ActionListener, Win
 			leaderboard.add(new LeaderboardPlayer(name, rating, wins, losses,rank));
 			displayboard.add(new LeaderboardPlayer(name, rating, wins, losses, rank));
 		}
-		updateTable();
+		repaint();
 	}
 
 	private class Panel extends JPanel
 	{
 		public Panel()
 		{
+			setLayout(null);
 
-			
-			GroupLayout layout = new GroupLayout(this);
-			setLayout(layout);
-			layout.setAutoCreateGaps(true);
-			layout.setAutoCreateContainerGaps(true);
-			
-			title = new JLabel("Leaderboard");
-			
-			refresh = new JButton("Refresh");
-			//refresh.setSize(150,50);
-			//refresh.setLocation(Leaderboard.this.getWidth()-200,Leaderboard.this.getHeight()-100);
+			refresh.setSize(150,50);
+			refresh.setLocation(Leaderboard.this.getWidth()-200,Leaderboard.this.getHeight()-100);
 			refresh.addActionListener(Leaderboard.this);
 			add(refresh);
 			refresh.doClick();
-			
-			search = new JTextField();
-			//search.setSize(300,20);
-			//search.setLocation(70,5);
+
+			search.setSize(300,20);
+			search.setLocation(70,5);
 			search.addKeyListener(Leaderboard.this);
-			search.addFocusListener(Leaderboard.this);
 			add(search);
 			
-			back = new JButton("Back");
-			back.addActionListener(Leaderboard.this);
-			
-
-			table = new JTable(data, columnNames);
-			
-			table.getColumnModel().getColumn(0).setWidth(60);
-			table.getColumnModel().getColumn(1).setWidth(300);
-			table.getColumnModel().getColumn(2).setWidth(100);
-			table.getColumnModel().getColumn(3).setWidth(100);
-			table.getColumnModel().getColumn(4).setWidth(100);
-			table.setRowSelectionAllowed(true);
-			scrollPane = new JScrollPane(table);
-			table.getTableHeader().setReorderingAllowed(false);
-			table.setFillsViewportHeight(true);
-
-			add(scrollPane);
-			
-			layout.setHorizontalGroup(
-					layout.createSequentialGroup()
-						.addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING)
-							.addGroup(layout.createSequentialGroup()
-								.addComponent(title)
-								.addGap(600)
-								.addComponent(search))
-							.addComponent(scrollPane)
-							.addGroup(layout.createSequentialGroup()
-								.addComponent(refresh)
-								.addGap(700)
-								.addComponent(back)))
-			);
-			layout.setVerticalGroup(
-					layout.createSequentialGroup()
-						.addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
-							.addComponent(title)
-							.addComponent(search))
-						.addComponent(scrollPane,400,400,400)
-						.addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
-							.addComponent(refresh)
-							.addComponent(back))
-			);
+			searchL.setSize(60,20);
+			searchL.setLocation(10,5);
+			add(searchL);
 		}
 
-		//@Override
-		/*public void paintComponent(Graphics graphics)
+		@Override
+		public void paintComponent(Graphics graphics)
 		{
 			super.paintComponent(graphics);
 			graphics.setColor(Color.black);
@@ -284,41 +207,7 @@ public class Leaderboard extends JFrame implements Runnable, ActionListener, Win
 					graphics.drawString(String.format("%3d. %-25s R:%4d %3dW %3dL", displayboard.get(i).getRank(), displayboard.get(i).getName(), displayboard.get(i).getRating(),displayboard.get(i).getWins(), displayboard.get(i).getLosses()), 50, y+=delta);
 				}
 			}
-		}*/
-	}
-	
-	public void updateTable(){
-		int tableHeight = Math.min(LEADERBOARD_DISPLAY_SIZE, displayboard.size());
-		data = new Object[tableHeight][5];
-
-		synchronized(leaderboard)
-		{
-			for(int i = 0; i < tableHeight;i++)
-			{
-				data[i][0] = new Integer(displayboard.get(i).getRank());
-				data[i][1] = displayboard.get(i).getName();
-				data[i][2] = new Integer(displayboard.get(i).getRating());
-				data[i][3] = new Integer(displayboard.get(i).getWins());
-				data[i][4] = new Integer(displayboard.get(i).getLosses());
-				
-				/*model.setValueAt(new Integer(displayboard.get(i).getRank()), i, 0);
-				model.setValueAt(displayboard.get(i).getName(), i, 1);
-				model.setValueAt(new Integer(displayboard.get(i).getRating()), i, 2);
-				model.setValueAt(new Integer(displayboard.get(i).getWins()), i, 3);
-				model.setValueAt(new Integer(displayboard.get(i).getLosses()), i, 4);*/
-				
-				System.out.println(displayboard.get(i).getRank());
-				System.out.println(displayboard.get(i).getName());
-				//graphics.drawString(String.format("%3d. %-25s R:%4d %3dW %3dL", displayboard.get(i).getRank(), displayboard.get(i).getName(), displayboard.get(i).getRating(),displayboard.get(i).getWins(), displayboard.get(i).getLosses()), 50, y+=delta);
-			}
 		}
-		
-		table = new JTable(data, columnNames);
-		
-		revalidate();
-		repaint();
-
-		
 	}
 
 	@Override
@@ -338,18 +227,9 @@ public class Leaderboard extends JFrame implements Runnable, ActionListener, Win
 			if(displayboard.size() > LEADERBOARD_DISPLAY_SIZE)
 				break;
 		}
-		updateTable();
+		repaint();	
 		
 	}
-	public void focusLost(FocusEvent e) {
-        if(search.getText().trim().equals(""))
-           search.setText("Search Player Names");
-    }
-	
-	public void focusGained(FocusEvent e) {
-        if(search.getText().trim().equals("Search Player Names"))
-           search.setText("");
-    }
 
 	@Override
 	public void keyTyped(KeyEvent arg0) {	
