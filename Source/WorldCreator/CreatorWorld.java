aab4928c591f37a194d5d0e072629d6023618525
Alex <alex.raita@gmail.com>
2017-01-15 14:32:30 -0500

    Expanded map. Fixed bugs in creator

--- Source/WorldCreator/CreatorWorld.java
+++ Source/WorldCreator/CreatorWorld.java
@@ -362,7 +362,7 @@ ActionListener, MouseWheelListener, MouseListener, MouseMotionListener
 		if (highlightingArea)
 		{
 			canDrawObject = false;
-			graphics.setColor(Color.white);
+			graphics.setColor(Color.green);
 
 			// If we are trying to selected an area that exceeds the grid, draw
 			// a smaller grid
@@ -428,7 +428,7 @@ ActionListener, MouseWheelListener, MouseListener, MouseMotionListener
 				&& selectedBlock[1] <= endColumn
 				&& isEditable)
 		{
-			graphics.setColor(Color.white);
+			graphics.setColor(Color.green);
 
 			// If we are highlighting  single tile
 			if(tiles[selectedTile].isTile())
@@ -469,7 +469,7 @@ ActionListener, MouseWheelListener, MouseListener, MouseMotionListener
 		}
 		else canDrawObject = false;
 
-		graphics.setColor(Color.white);
+		graphics.setColor(Color.green);
 
 		// Draw an outline
 		graphics.drawRect((int) (CENTRE_X - posX), (int) (CENTRE_Y  - posY),(int)((grid[0].length)
@@ -501,7 +501,7 @@ ActionListener, MouseWheelListener, MouseListener, MouseMotionListener
 	 */
 	public boolean canFit(int startRow, int startCol, int width, int height, boolean isTile)
 	{
-		if(startCol + width >= grid[0].length ||startRow + height >= grid.length)
+		if(startCol + width > grid[0].length ||startRow + height > grid.length)
 			return false;
 		for(CreatorWorldObject object : objects)
 		{
