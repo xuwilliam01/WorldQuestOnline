0c9c2b74ce14f46221032e8da00b288ff22eff7d
William <xuwilliam01@gmail.com>
2018-03-03 03:19:24 -0800

    Started working on player a.i.

--- Source/Server/Creatures/ServerAiPlayer.java
+++ Source/Server/Creatures/ServerAiPlayer.java
@@ -8,23 +8,16 @@ import Server.ServerWorld;
  * @author William Xu & Alex Raita
  *
  */
-public class ServerAiPlayer extends ServerCreature implements Runnable {
+public class ServerAiPlayer extends ServerCreature{
 
 	public ServerAiPlayer(double x, double y, int width, int height, double relativeDrawX, double relativeDrawY,
 			double gravity, String image, String type, int maxHP, ServerWorld world, boolean attackable) {
 		super(x, y, width, height, relativeDrawX, relativeDrawY, gravity, image, type, maxHP, world, attackable);
-		// TODO Auto-generated constructor stub
-	}
-
-	@Override
-	public void run() {
-		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
 	public void update() {
-		// TODO Auto-generated method stub
 		
 	}
 }
