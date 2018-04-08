4bc37e1998d719fae53d85db6a34c042ee164302
William <xuwilliam01@gmail.com>
2018-04-07 22:37:48 -0700

    Updated jars and now use StartServer to start central

--- Source/START/StartServer.java
+++ Source/START/StartServer.java
@@ -1,11 +1,15 @@
 package START;
 
+import java.io.IOException;
 import java.net.SocketException;
 import java.time.Clock;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
 
+import org.jdom2.JDOMException;
+
+import CentralServer.CentralServer;
 import ClientUDP.ClientAccountWindow;
 import Imports.Audio;
 import Imports.GameImage;
@@ -18,46 +22,55 @@ public class StartServer {
 
 	final static int MAX_ROOMS = 1;
 
-	public static void main(String[] args) throws SocketException {
+	public static void main(String[] args) throws IOException, JDOMException {
 		Scanner scan = new Scanner(System.in);
 		System.out.print("Enter server name: ");
 		String name = scan.nextLine();
 		name = name.replace(' ', '_');
-		
-		System.out.print("Enter central server IP: ");
-		ClientAccountWindow.Domain = scan.nextLine();
-		scan.close();
-		
-		Imports.Audio.isServer=true;
-		
-		GameImage.hostingServer=true;
-		Images.importImages();
-		Audio.importAudio(false);
-		GameMaps.importMaps();
-
-		
-		ServerManager server = new ServerManager(name, MainMenu.DEF_PORT, MAX_ROOMS, false, true);
-		Thread serverThread = new Thread(server);
-		serverThread.start();
-
-		System.out.println("Done loading");
-		
-		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
-		LocalDateTime now = LocalDateTime.now(Clock.systemUTC());
-		System.out.println(dtf.format(now) + " UTC");
-
-//		scan = new Scanner(System.in);
-//		
-//		while (true) {
-//			if (scan.hasNextLine()) {
-//				if (scan.nextLine().equalsIgnoreCase("exit"))
-//				{
-//					System.out.println("Server closed");
-//					scan.close();
-//					System.exit(0);
-//				}
-//			}
-//		}
+		if (name.equals("central"))
+		{
+			CentralServer server = new CentralServer();
+			Thread serverThread = new Thread(server);
+			serverThread.start();
+			System.out.println("Central server started");
+		}
+		else
+		{
+			System.out.print("Enter central server IP: ");
+			ClientAccountWindow.Domain = scan.nextLine();
+			scan.close();
+			
+			Imports.Audio.isServer=true;
+			
+			GameImage.hostingServer=true;
+			Images.importImages();
+			Audio.importAudio(false);
+			GameMaps.importMaps();
+	
+			
+			ServerManager server = new ServerManager(name, MainMenu.DEF_PORT, MAX_ROOMS, false, true);
+			Thread serverThread = new Thread(server);
+			serverThread.start();
+	
+			System.out.println("Done loading");
+			
+			DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+			LocalDateTime now = LocalDateTime.now(Clock.systemUTC());
+			System.out.println(dtf.format(now) + " UTC");
+	
+	//		scan = new Scanner(System.in);
+	//		
+	//		while (true) {
+	//			if (scan.hasNextLine()) {
+	//				if (scan.nextLine().equalsIgnoreCase("exit"))
+	//				{
+	//					System.out.println("Server closed");
+	//					scan.close();
+	//					System.exit(0);
+	//				}
+	//			}
+	//		}
+		}
 	}
 
 }
