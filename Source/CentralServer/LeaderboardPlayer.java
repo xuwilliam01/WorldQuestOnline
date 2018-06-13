7d7fcc66e0a269b2474f56c58e6672435fc1d2fc
William <xuwilliam01@gmail.com>
2018-06-13 19:17:16 -0400

    Fixed ordering of priority queue

--- Source/CentralServer/LeaderboardPlayer.java
+++ Source/CentralServer/LeaderboardPlayer.java
@@ -19,7 +19,7 @@ public class LeaderboardPlayer implements Comparable<LeaderboardPlayer>{
 	@Override
 	public int compareTo(LeaderboardPlayer o) {
 		// TODO Auto-generated method stub
-		return rating - o.rating;
+		return o.rating - rating;
 	}
 	public int getRating() {
 		return rating;
